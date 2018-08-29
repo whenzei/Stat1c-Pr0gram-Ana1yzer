9719c8547e91ffd7dab725998a0611d463c6b47f
Lau Kar Rui <karrui.lau@gmail.com>
2018-08-29 17:44:04 +0800

    feat(Parser): add ability to read content from file
    Validation check for the readContentFromFile() function is also included, and will return empty string if file does not exist

--- source/Parser.cpp
+++ source/Parser.cpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <stdio.h>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,8 +10,36 @@ using namespace std;
 
 #include "PKB.h"
 #include "TNode.h"
+#include "Parser.h"
+#include "Tokenizer.h"
 
-int Parse () {
+Parser::Parser(string filepath) {
+	this->content = readContentFromFile(filepath);
+	parse();
+}
 
-	return 0;
+
+string Parser::readContentFromFile(string filepath)
+{	
+	if (!isValidFile(filepath)) {
+		cout << "File not found! Content is set to empty string" << endl;
+		return "";
+	}
+	// start reading file
+	ifstream ifs(filepath);
+	string content((istreambuf_iterator<char>(ifs)),
+		(istreambuf_iterator<char>()));
+	return content;
+}
+
+void Parser::parse() {
+	// check whether it is procedure (as we are currently guaranteed to only have 1 procedure)
+	vector<string> tokenizedContent = Tokenizer::tokenize(this->content);
+	for (vector<string>::const_iterator i = tokenizedContent.begin(); i != tokenizedContent.end(); ++i)
+		std::cout << *i << "<--- \n";
+}
+
+bool Parser::isValidFile(string filepath) {
+	ifstream infile(filepath);
+	return infile.good();
 }
--- source/Parser.h
+++ source/Parser.h
@@ -1,3 +1,41 @@
+
 #pragma once
-using namespace std;
-int Parse();
+
+#include <vector>
+
+#ifndef SPA_PARSER_H
+#define SPA_PARSER_H
+
+/**
+ * The parser class for the front end component
+ * Used to parse file contents and pass the parsed content to the PKB component
+ */
+class Parser {
+	std::string content;
+public:
+	/* Constructor */
+	Parser(std::string filepath);
+
+	///* Destructor */
+	//~Parser();
+
+	/**
+	 * Reads the content of the file at the location specified by the file path
+	 * and returns the file contents if available
+	 * @param filepath the filepath of the file to be parsed
+	 * @returns file content as a string if file exists, or empty string otherwise
+	 */
+	std::string readContentFromFile(std::string filepath);
+
+	void parse();
+
+private:
+	/**
+	 * Checks if file can be located at given file path
+	 * @param filepath the filepath of the file to be located
+	 * @returns true if file exists, false otherwise
+	*/
+	bool isValidFile(std::string filepath);
+};
+	
+#endif // !SPA_PARSER_H
