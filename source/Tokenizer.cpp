f8e51f0838d5ba996ce9814c30647ad894c7ea5f
Lau Kar Rui <karrui.lau@gmail.com>
2018-08-29 17:47:07 +0800

    feat(Tokenizer): add preliminary tokenizer to split by new lines
    This is not sustainable as a SIMPLE program need not be split by new lines, but it is worth a good first attempt as a framework

---
+++ source/Tokenizer.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <regex>
+
+#include "Tokenizer.h"
+
+using namespace std;
+
+vector<string> Tokenizer::tokenize(string input)
+{
+	vector<string> tokens;
+	regex regex{ R"([\r\n]+)" }; // split on space and comma
+	sregex_token_iterator it{ input.begin(), input.end(), regex, -1 };
+	tokens = { it, {} };
+	return tokens;
+}
---
+++ source/Tokenizer.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef SPA_TOKENIZER_H
+#define SPA_TOKENIZER_H
+
+class Tokenizer {
+public:
+	static std::vector<std::string> tokenize(std::string input);
+};
+
+#endif // !SPA_TOKENIZER_H
