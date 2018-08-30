0dff84a259d87eae1be6d4853ec49b049b430e6a
Lau Kar Rui <karrui.lau@gmail.com>
2018-08-30 16:59:11 +0800

    refactor: move import and using statements inside definition

--- source/tokenizer.h
+++ source/tokenizer.h
@@ -1,14 +1,18 @@
 #pragma once
 
-#include <regex>
-#include <vector>
-
 #ifndef SPA_TOKENIZER_H
 #define SPA_TOKENIZER_H
 
+#include <regex>
+#include <vector>
+
+using std::regex;
+using std::string;
+using std::vector;
+
 struct Token {
   int type;
-  std::string value;
+  string value;
 };
 
 struct Result {
@@ -17,34 +21,34 @@ struct Result {
 };
 
 class Tokenizer {
-  typedef Result (*TokenizerFunc)(std::string, int);
+  typedef Result (*TokenizerFunc)(string, int);
   int num_functions;
 
  public:
-  static std::vector<Token> Tokenize(std::string input);
+  static vector<Token> Tokenize(string input);
 
   /// Debug function, prints out the contents of the token
   /// @returns formatted string of token type and value
-  static std::string Debug(Token);
+  static string Debug(Token);
 
  private:
-  static Result TokenizeCharacter(int type, char value, std::string input,
+  static Result TokenizeCharacter(int type, char value, string input,
                                   int current_index = 0);
 
-  static Result TokenizePattern(int type, std::regex pattern, std::string input,
+  static Result TokenizePattern(int type, regex pattern, string input,
                                 int current_index = 0);
 
-  static Result SkipWhitespace(std::string input, int current_index = 0);
+  static Result SkipWhitespace(string input, int current_index = 0);
 
-  static Result TokenizeDigits(std::string input, int current_index = 0);
+  static Result TokenizeDigits(string input, int current_index = 0);
 
-  static Result TokenizeNames(std::string input, int current_index = 0);
+  static Result TokenizeNames(string input, int current_index = 0);
 
-  static Result TokenizeBraces(std::string input, int current_index = 0);
+  static Result TokenizeBraces(string input, int current_index = 0);
 
-  static Result TokenizeSemicolon(std::string input, int current_index = 0);
+  static Result TokenizeSemicolon(string input, int current_index = 0);
 
-  static Result TokenizeEquals(std::string input, int current_index = 0);
+  static Result TokenizeEquals(string input, int current_index = 0);
 };
 
 #endif  // !SPA_TOKENIZER_H
