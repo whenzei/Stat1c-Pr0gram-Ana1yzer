0f7f611333d2d65f12593435d2db2e40642ee5a5
KEVIN CHIN <kevinchinjh@gmail.com>
2018-08-31 21:08:26 +0800

    feat(QueryEvaluator): add new method to format the result string

--- SPA/query_evaluator.cpp
+++ SPA/query_evaluator.cpp
@@ -1,38 +1,78 @@
+#include <list>
 #include <string>
 #include <vector>
 
 #include "declaration.h"
 #include "query.h"
 #include "query_evaluator.h"
+#include "stmt_table.h"
+#include "var_table.h"
 
-QueryEvaluator::QueryEvaluator() {}
+QueryEvaluator::QueryEvaluator() { select_type = DeclarationKeyword; }
 
-std::string QueryEvaluator::GetResultFromQuery(Query query) {
+std::list<std::string> QueryEvaluator::GetResultFromQuery(Query query) {
   std::string select_var_name = query.GetVarName();
   std::vector<Declaration> declarations = query.GetDeclarations();
-  std::string result = "none";
+  std::list<std::string> results;
 
   // Find out what the user is selecting by going through the list of
   // declarations made by the user
   for (auto &iter : declarations) {
     // If there is a match between the selection and declaration
     if (select_var_name.compare(iter.GetVarName()) == 0) {
-      DeclarationKeyword select_type = iter.GetKeyword();
+      select_type = iter.GetKeyword();
 
       switch (select_type) {
         case DeclarationKeyword::kProcedure:
-          // Get all procedures name from PKB and store into result string
+          // Get all procedures name from PKB and store into results list
           break;
         case DeclarationKeyword::kVariable:
-          // Get all variable name from PKB and store into result string
+          // Get all variable name from PKB and store into results list
           break;
         case DeclarationKeyword::kAssign:
           // Get all statement number of statement which contains assignment
-          // from PKB and store into result string
+          // from PKB and store into results list
           break;
       }
       break;
     }
   }
-  return result;
+  return results;
+}
+
+std::string FormatResultString(std::list<std::string> results) {
+  std::string formatted_string;
+  std::string index_flag = "";
+
+  if (results.empty()) {
+    // If no result, output none
+    formatted_string = "none";
+  } else {
+    // Check the type of the result
+    switch (select_type) {
+      case DeclarationKeyword::kProcedure:
+        // Format for procedure
+        break;
+      case DeclarationKeyword::kVariable:
+        // Format for variable
+        break;
+      case DeclarationKeyword::kAssign:
+        index_flag = "#";
+        formatted_string = "statement ";
+        break;
+    }
+
+    // Loop through the results and format it into a single string
+    for (auto &iter : results) {
+      formatted_string += index_flag + iter;
+
+      if (iter != results.back()) {
+        formatted_string += ", ";
+      } else {
+        formatted_string += ".";
+      }
+    }
+  }
+
+  return formatted_string;
 }
--- SPA/query_evaluator.h
+++ SPA/query_evaluator.h
@@ -1,26 +1,40 @@
 #pragma once
 
+#include <list>
 #include <string>
 #include <vector>
 
+#include "declaration.h"
 #include "query.h"
 
 #ifndef QUERY_EVALUATOR_H
 #define QUERY_EVALUATOR_H
 
+/*A class to evaluate user query and return result to user*/
 class QueryEvaluator {
+ private:
+  DeclarationKeyword select_type;
+
  public:
   /* Contructor */
   QueryEvaluator();
 
   /**
-   * Use the Query provided by user
-   * and return a result string based on the query
+   * Use the @Query provided by user
+   * and return a list of results based on the query
    * @param query the query by the user
-   * @returns result as a string if there is a result, or "none"
-   * otherwise
+   * @returns a list of string if there is result,
+   * or an empty list otherwise
    */
-  std::string GetResultFromQuery(Query query);
+  std::list<std::string> GetResultFromQuery(Query query);
+
+  /**
+   * Use the results from @GetResultFromQuery
+   * and format it into a single string to display to the user
+   * @param results the list of string containing result of the query
+   * @returns a string to be displayed to the user
+   */
+  std::string FormatResultString(std::list<std::string> results);
 };
 
 #endif  // !QUERY_EVALUATOR_H
