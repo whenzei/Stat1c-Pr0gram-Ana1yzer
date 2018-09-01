d282d8ee9da230db2967cc7ca5a784bc53f62782
sophie <e0008892@u.nus.edu>
2018-09-01 19:02:44 +0800

    feat(StmtTable): update statement table functions

--- SPA/stmt_table.cpp
+++ SPA/stmt_table.cpp
@@ -1,23 +1,34 @@
 #include "stmt_table.h"
+#include "statement.h"
 
 #include <string>
 
 using std::string;
 
-const int kTempArraySize = 10;
-
 StmtTable::StmtTable() {
-	stmt_table = new string[kTempArraySize];
+	num_stmt = 0;
 }
 
-int StmtTable::InsertStmt(string stmt) {
-  return 0;
+StmtTable::~StmtTable() {
+	delete[] * stmt_table;
 }
 
-string StmtTable::GetStmt(int stmt_num) {
-	return stmt_table[stmt_num];
+// Prerequisite: The line number and statement are valid and non-null.
+int StmtTable::InsertStmt(int line_num, string stmt) {
+	Statement newStmt(line_num, stmt);
+	*stmt_table[num_stmt] = newStmt;
+	num_stmt++;
+  return num_stmt;
 }
 
-string* StmtTable::GetAllStmt() {
-	return stmt_table;
+int* StmtTable::GetAllStmtNums() {
+
+	int stmt_nums[kTempArraySize];
+	
+	if (num_stmt == 0) { return stmt_nums;  }
+	for (int i = 0; i < num_stmt; i++) {
+		stmt_nums[i] = stmt_table[i]->getLineNum();
+	}
+
+	return stmt_nums;
 }
--- SPA/stmt_table.h
+++ SPA/stmt_table.h
@@ -1,28 +1,34 @@
 #pragma once
+#include "statement.h"
 
 #include <string>
 
 using std::string;
+
+const int kTempArraySize = 10;
 
 #ifndef STMT_TABLE_H
 #define STMT_TABLE_H
 
 class StmtTable {
 private:
-	string* stmt_table;
+	Statement* stmt_table[kTempArraySize];
+	int num_stmt;
 
 public:
 	// Constructor
 	StmtTable();
+
+	// Destructor
+	~StmtTable();
+	
 	// Takes in the statement in string format and inserts it in the StmtTable.
 	// Returns the index of the statement.
-	int InsertStmt(string stmt);
-	// Returns the statement with its index as the given statement number.
-	// Returns -1 if given index is empty.
-	string GetStmt(int stmt_num);
-	// Returns all statements in the StmtTable.
-	// Returns null if StmtTable is empty. 
-	string* GetAllStmt();
+	int InsertStmt(int line_num, string stmt);
+
+	// Returns a pointer to a list of all line numbers of all statements in the StmtTable.
+	// Returns a pointer to an empty list if StmtTable is empty. 
+	int* GetAllStmtNums();
 };
 
 #endif !STMT_TABLE_H
