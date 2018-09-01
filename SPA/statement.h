04695cc55da05227dd592290271563f039395c31
sophie <e0008892@u.nus.edu>
2018-09-01 19:01:44 +0800

    feat(StmtTable): add statement class

---
+++ SPA/statement.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+
+using std::string;
+
+#ifndef STATEMENT_H
+#define STATEMENT_H
+
+class Statement {
+private:
+	string content;
+	int line_num;
+
+public: 
+	// Default constructor
+	Statement() {};
+
+	// Constructor
+	Statement(int index, string stmt) {
+		line_num = index;
+		content = stmt;
+	}
+
+	// Accessors
+	string getContent() {
+		return content;
+	}
+
+	int getLineNum() {
+		return line_num;
+	}
+};
+
+#endif !STATEMENT_H
