bccb59bc5cd86fdd556ae84d193b5a88bf09bd55
sophie <e0008892@u.nus.edu>
2018-09-02 19:32:55 +0800

    refactor: change indentation

--- source/stmt_table.cpp
+++ source/stmt_table.cpp
@@ -6,29 +6,28 @@
 using std::string;
 
 StmtTable::StmtTable() {
-	num_stmt = 0;
+  num_stmt = 0;
 }
 
 StmtTable::~StmtTable() {
-	delete[] * stmt_table;
+  delete[] * stmt_table;
 }
 
 // Prerequisite: The line number and statement are valid and non-null.
 int StmtTable::InsertStmt(int line_num, string stmt) {
-	Statement newStmt(line_num, stmt);
-	*stmt_table[num_stmt] = newStmt;
-	num_stmt++;
+  Statement newStmt(line_num, stmt);
+  *stmt_table[num_stmt] = newStmt;
+  num_stmt++;
   return (num_stmt-1);
 }
 
 int* StmtTable::GetAllStmtNums() {
-
-	int stmt_nums[kTempArraySize];
+  int stmt_nums[kTempArraySize];
 	
-	if (num_stmt == 0) { return NULL;  }
-	for (int i = 0; i < num_stmt; i++) {
-		stmt_nums[i] = stmt_table[i]->GetLineNum();
-	}
+  if (num_stmt == 0) { return NULL;  }
+  for (int i = 0; i < num_stmt; i++) {
+    stmt_nums[i] = stmt_table[i]->GetLineNum();
+  }
 
-	return stmt_nums;
+  return stmt_nums;
 }
--- source/var_table.cpp
+++ source/var_table.cpp
@@ -5,20 +5,20 @@
 using std::string;
 
 VarTable::VarTable() {
-	num_var = 0;
+  num_var = 0;
 }
 
 VarTable::~VarTable() {
-	delete[] * var_table;
+  delete[] * var_table;
 }
 
 int VarTable::InsertVar(string var_name) {
-	*var_table[num_var] = var_name;
-	num_var++;
-	return (num_var-1);
+  *var_table[num_var] = var_name;
+  num_var++;
+  return (num_var-1);
 }
 
 string* VarTable::GetAllVar() {
-	if (num_var == 0) { return NULL; }
-	else { return *var_table; }
+  if (num_var == 0) { return NULL; }
+  else { return *var_table; }
 }
