1b1fc29ed8a2bebef0092a271226f7660113ac7f
Ken <kenpaxtonlim@outlook.com>
2018-09-03 12:18:10 +0800

    style: fix indentation

--- UnitTesting/test_query_parser.cpp
+++ UnitTesting/test_query_parser.cpp
@@ -6,32 +6,32 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using std::string;
 
 namespace UnitTesting {
-  /*
-  This test the parsing logic for PQL.
-  */
-	TEST_CLASS(TestQueryParser) {
-    public:
-    TEST_METHOD(TestEmptyQuery) {
-	    string content = "";
-	    Query query(content);
-	    Assert::IsTrue(query.GetDeclarations().empty());
-	    Assert::IsTrue(query.GetVarName() == "");
-    }
+/*
+This test the parsing logic for PQL.
+*/
+TEST_CLASS(TestQueryParser) {
+public:
+  TEST_METHOD(TestEmptyQuery) {
+	  string content = "";
+	  Query query(content);
+	  Assert::IsTrue(query.GetDeclarations().empty());
+	  Assert::IsTrue(query.GetVarName() == "");
+  }
 
-    TEST_METHOD(TestAssign) {
-	    string content = "assign a;";
-	    Query query(content);
-	    Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
-	    Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
-	    Assert::IsTrue(query.GetVarName() == "");
-    }
+  TEST_METHOD(TestAssign) {
+	  string content = "assign a;";
+	  Query query(content);
+	  Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
+	  Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
+	  Assert::IsTrue(query.GetVarName() == "");
+  }
 
-    TEST_METHOD(TestAssignSelect) {
-	    string content = "assign a; Select a;";
-	    Query query(content);
-	    Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
-	    Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
-	    Assert::IsTrue(query.GetVarName() == "a");
-    }
-    };
+  TEST_METHOD(TestAssignSelect) {
+	  string content = "assign a; Select a;";
+	  Query query(content);
+	  Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
+	  Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
+	  Assert::IsTrue(query.GetVarName() == "a");
+  }
+  };
 }
