4fa93ac690dca53316dc8a0299d3bc26c30f3a31
sophie <e0008892@u.nus.edu>
2018-09-03 18:53:43 +0800

    fix: fix pointers and change return type of GetAllStmtNum

--- source/stmt_table.cpp
+++ source/stmt_table.cpp
@@ -12,28 +12,25 @@ StmtTable::StmtTable() {
   num_stmt_ = 0;
 }
 
-StmtTable::~StmtTable() {
-  delete[] * stmt_table_;
-}
-
 int StmtTable::InsertStmt(int line_num, string stmt) {
   for (int i = 0; i < num_stmt_; i++) {
-    if (stmt_table_[i]->GetLineNum() == line_num) { return kFail; }
+    if ((stmt_table_[i]).GetLineNum()==line_num) { return kFail; }
   }
 
   Statement newStmt(line_num, stmt);
-  *stmt_table_[num_stmt_] = newStmt;
+  stmt_table_[num_stmt_] = newStmt;
   num_stmt_++;
   return kSuccess;
 }
 
-int* StmtTable::GetAllStmtNums() {
+std::vector<int> StmtTable::GetAllStmtNums() {
   int stmt_nums[kTempArraySize];
+  std::vector<int> stmt_num;
 	
-  if (num_stmt_ == 0) { return NULL;  }
   for (int i = 0; i < num_stmt_; i++) {
-    stmt_nums[i] = stmt_table_[i]->GetLineNum();
+    stmt_num.push_back((stmt_table_[i]).GetLineNum());
+    stmt_nums[i] = (stmt_table_[i]).GetLineNum();
   }
 
-  return stmt_nums;
+  return stmt_num;
 }
