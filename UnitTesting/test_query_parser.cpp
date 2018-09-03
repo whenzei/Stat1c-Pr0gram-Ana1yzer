27dc9fde6302eeb47e114578cae3e0f3c9b43aaa
Ken <kenpaxtonlim@outlook.com>
2018-09-03 11:58:45 +0800

    test(pql_parser): add unit testing for pql parser

---
+++ UnitTesting/test_query_parser.cpp
@@ -0,0 +1,34 @@
+#include "CppUnitTest.h"
+#include "stdafx.h"
+#include "query.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using std::string;
+
+namespace UnitTesting {
+	TEST_CLASS(TestQueryParser) {
+public:
+	TEST_METHOD(TestEmptyQuery) {
+		string content = "";
+		Query query(content);
+		Assert::IsTrue(query.GetDeclarations().empty());
+		Assert::IsTrue(query.GetVarName() == "");
+	}
+
+	TEST_METHOD(TestAssign) {
+		string content = "assign a;";
+		Query query(content);
+		Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
+		Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
+		Assert::IsTrue(query.GetVarName() == "");
+	}
+
+	TEST_METHOD(TestAssignSelect) {
+		string content = "assign a; Select a;";
+		Query query(content);
+		Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
+		Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
+		Assert::IsTrue(query.GetVarName() == "a");
+	}
+	};
+}
