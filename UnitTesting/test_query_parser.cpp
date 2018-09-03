331a1dca66697f919733bd8abc0b33964dd004c8
Ken <kenpaxtonlim@outlook.com>
2018-09-03 12:16:32 +0800

    style: indentation for test_query_parser

--- UnitTesting/test_query_parser.cpp
+++ UnitTesting/test_query_parser.cpp
@@ -10,28 +10,28 @@ namespace UnitTesting {
   This test the parsing logic for PQL.
   */
 	TEST_CLASS(TestQueryParser) {
-  public:
-	TEST_METHOD(TestEmptyQuery) {
-		string content = "";
-		Query query(content);
-		Assert::IsTrue(query.GetDeclarations().empty());
-		Assert::IsTrue(query.GetVarName() == "");
-	}
+    public:
+    TEST_METHOD(TestEmptyQuery) {
+	    string content = "";
+	    Query query(content);
+	    Assert::IsTrue(query.GetDeclarations().empty());
+	    Assert::IsTrue(query.GetVarName() == "");
+    }
 
-	TEST_METHOD(TestAssign) {
-		string content = "assign a;";
-		Query query(content);
-		Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
-		Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
-		Assert::IsTrue(query.GetVarName() == "");
-	}
+    TEST_METHOD(TestAssign) {
+	    string content = "assign a;";
+	    Query query(content);
+	    Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
+	    Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
+	    Assert::IsTrue(query.GetVarName() == "");
+    }
 
-	TEST_METHOD(TestAssignSelect) {
-		string content = "assign a; Select a;";
-		Query query(content);
-		Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
-		Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
-		Assert::IsTrue(query.GetVarName() == "a");
-	}
-	};
+    TEST_METHOD(TestAssignSelect) {
+	    string content = "assign a; Select a;";
+	    Query query(content);
+	    Assert::IsTrue(query.GetDeclarations()[0].GetKeyword() == DeclarationKeyword::kAssign);
+	    Assert::IsTrue(query.GetDeclarations()[0].GetVarName() == "a");
+	    Assert::IsTrue(query.GetVarName() == "a");
+    }
+    };
 }
