fc267f980ad32dc5b14111bc821afad56291c2e8
KEVIN CHIN <kevinchinjh@gmail.com>
2018-09-04 00:28:08 +0800

    feat(integrate pql to autotester and gui)

--- AutoTester/source/test_wrapper.cpp
+++ AutoTester/source/test_wrapper.cpp
@@ -1,6 +1,7 @@
 #include "test_wrapper.h"
 #include "parser.h"
 #include "pql_query.h"
+#include "query_evaluator.h"
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -34,6 +35,8 @@ void TestWrapper::evaluate(std::string query, std::list<std::string>& results) {
   // call your evaluator to evaluate the query here
   // ...code to evaluate query...
   PqlQuery pql_query(query);
+  QueryEvaluator qe;
   // store the answers to the query in the results list (it is initially empty)
   // each result must be a string.
+  results = qe.GetResultFromQuery(query, pkb_);
 }
--- Gui/gui_wrapper.cpp
+++ Gui/gui_wrapper.cpp
@@ -1,5 +1,7 @@
 #include "gui_wrapper.h"
 #include "../source/parser.h"
+#include "../source/pql_query.h"
+#include "../source/query_evaluator.h"
 
 #include <iostream>
 
@@ -23,9 +25,12 @@ void GUIWrapper::parse(std::string filename) {
 void GUIWrapper::evaluate(std::string query, std::list<std::string>& results) {
   // call your evaluator to evaluate the query here
   // ...code to evaluate query...
-
+  PqlQuery pql_query(query);
+  QueryEvaluator qe;
   std::cout << "query=  " << query << std::endl;
-  results.push_back(query + "\nQuery result: ...");
+
+  //results.push_back(query + "\nQuery result: ...");
   // store the answers to the query in the results list (it is initially empty)
   // each result must be a string.
+  results = qe.GetResultFromQuery(query, pkb_);
 }
