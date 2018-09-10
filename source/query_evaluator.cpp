385c8ffa3380e4639ac3f0b2b20a9ce648217fc7
KEVIN CHIN <kevinchinjh@gmail.com>
2018-09-10 19:19:42 +0800

    refractor(removed std::copy for select all method): directly return the results since all of pkb methods are now list<string>

--- source/query_evaluator.cpp
+++ source/query_evaluator.cpp
@@ -24,7 +24,17 @@ list<string> QueryEvaluator::GetResultFromQuery(PqlQuery* query, PKB pkb) {
   string select_var_name = query->GetVarName();
   map<string, PqlDeclarationEntity> declarations = query->GetDeclarations();
 
-  return GetResultFromSelectAllQuery(select_var_name, declarations, pkb);
+  list<string> results;
+  results = GetResultFromSelectAllQuery(select_var_name, declarations, pkb);
+
+  cout << "Result size: " << results.size() << endl;
+
+  // Set result to none if no results were found.
+  if (results.size() == 0) {
+    results.push_back("none");
+  }
+
+  return results;
 }
 
 list<string> QueryEvaluator::GetResultFromSelectAllQuery(
@@ -39,89 +49,52 @@ list<string> QueryEvaluator::GetResultFromSelectAllQuery(
     // Check for a match between the selection and declaration
     if (select_var_name.compare(it->first) == 0) {
       select_type = it->second;
-      list<string> result_list;
 
       switch (select_type) {
         case PqlDeclarationEntity::kProcedure:
           // Get all procedures name from PKB and store into results list
-          result_list = pkb.GetAllProcName();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all procedure." << endl;
-          break;
+          return pkb.GetAllProcName();
         case PqlDeclarationEntity::kVariable:
           // Get all variable name from PKB and store into results list
-          result_list = pkb.GetAllVarName();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all variables." << endl;
-          break;
+          return pkb.GetAllVarName();
         case PqlDeclarationEntity::kAssign:
           // Get all statement number of statement which contains assignment
           // from PKB and store into results list
-          result_list = pkb.GetAllAssignStmt();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all assign statement." << endl;
-          break;
+          return pkb.GetAllAssignStmt();
         case PqlDeclarationEntity::kStmt:
           // Get all stmt number from PKB and store into results list
-          result_list = pkb.GetAllStmt();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all statement." << endl;
-          break;
+          return pkb.GetAllStmt();
         case PqlDeclarationEntity::kRead:
           // Get all read stmt from PKB and store into results list
-          result_list = pkb.GetAllReadStmt();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all read statement." << endl;
-          break;
+          return pkb.GetAllReadStmt();
         case PqlDeclarationEntity::kPrint:
           // Get all print stmt from PKB and store into results list
-          result_list = pkb.GetAllPrintStmt();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all print statement." << endl;
-          break;
+          return pkb.GetAllPrintStmt();
         case PqlDeclarationEntity::kCall:
           // Get all call stmt from PKB and store into results list
           break;
         case PqlDeclarationEntity::kWhile:
           // Get all while stmt from PKB and store into results list
-          result_list = pkb.GetAllWhileStmt();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all while statement." << endl;
-          break;
+          return pkb.GetAllWhileStmt();
         case PqlDeclarationEntity::kIf:
           // Get all if stmt from PKB and store into results list
-          result_list = pkb.GetAllIfStmt();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all if statement." << endl;
-          break;
+          return pkb.GetAllIfStmt();
         case PqlDeclarationEntity::kConstant:
           // Get all constant from PKB and store into results list
-          result_list = pkb.GetAllConstValue();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all constants." << endl;
-          break;
+          return pkb.GetAllConstValue();
         case PqlDeclarationEntity::kProgline:
           // Get all program line from PKB and store into results list
-          result_list = pkb.GetAllStmt();
-          std::copy(result_list.begin(), result_list.end(),
-                    std::back_inserter(results));
           cout << "Select all program lines." << endl;
-          break;
-      }
-      cout << "Result size: " << result_list.size() << endl;
-
-	  //Set result to none if no results were found.
-      if (result_list.size() == 0) {
-        results.push_back("none");
+          return pkb.GetAllStmt();
       }
       break;
     }
