5c55199e8705dd959ea73a6f3348d2d87821c563
DESKTOP-8R132I1\Zhen Wei <e0032088@u.nus.edu>
2018-09-16 00:16:30 +0800

    feat(ExpressionHelper): add new methods for postfix pattern matching

--- source/expression_helper.cpp
+++ source/expression_helper.cpp
@@ -56,3 +56,74 @@ TokenList ExpressionHelper::ToPostfix(TokenList tokens_to_convert) {
 
   return result_tokens;
 }
+
+bool ExpressionHelper::IsPatternStartWith(TokenList tokens_primary,
+                                          TokenList tokens_secondary) {
+  // tokens_primary should be >= tokens_secondary
+  if (tokens_primary.size() < tokens_secondary.size()) {
+    return false;
+  }
+
+  size_t end_index = tokens_secondary.size() - 1;
+  return Matches(tokens_primary, tokens_primary, 0, end_index);
+}
+
+bool ExpressionHelper::IsPatternEndWith(TokenList tokens_primary,
+                                        TokenList tokens_secondary) {
+  // tokens_primary should be >= tokens_secondary
+  if (tokens_primary.size() < tokens_secondary.size()) {
+    return false;
+  }
+  size_t end_index = tokens_secondary.size() - 1;
+  size_t start_index;
+
+  if (tokens_primary.size() == tokens_secondary.size()) {
+    start_index = 0;
+  } else {
+    start_index = tokens_primary.size() - tokens_secondary.size();
+  }
+  return Matches(tokens_primary, tokens_secondary, start_index, end_index);
+}
+
+bool ExpressionHelper::IsPatternExact(TokenList tokens_primary,
+                                      TokenList tokens_secondary) {
+  if (tokens_primary.size() != tokens_secondary.size()) {
+    return false;
+  }
+  size_t end_index = tokens_secondary.size() - 1;
+
+  return Matches(tokens_primary, tokens_secondary, 0, end_index);
+}
+
+bool ExpressionHelper::IsPatternSubSet(TokenList tokens_primary,
+                                       TokenList tokens_secondary) {
+  if (tokens_primary.size() < tokens_secondary.size()) {
+    return false;
+  }
+
+  size_t num_frames = tokens_primary.size() - tokens_secondary.size() + 1;
+  for (size_t i = 0; i < num_frames; i++) {
+    size_t start_index = i;
+    size_t end_index = tokens_secondary.size() - 1 + i;
+    bool match_status =
+        Matches(tokens_primary, tokens_secondary, start_index, end_index);
+    // Check if there is a matching frame
+				if (match_status) {
+      return true;
+				}
+  }
+
+		return false;
+}
+
+bool ExpressionHelper::Matches(TokenList tokens_first, TokenList tokens_second,
+                               size_t start, size_t end) {
+  for (size_t curr = start; start <= end; start++) {
+    string curr_pri_value = tokens_first[curr].value;
+    string curr_sec_value = tokens_second[curr].value;
+    if (curr_pri_value != curr_sec_value) {
+      return false;
+    }
+  }
+  return true;
+}
--- source/expression_helper.h
+++ source/expression_helper.h
@@ -7,6 +7,57 @@
 
 class ExpressionHelper {
  public:
+  // Implementation of shunting-yard algorithm
+  // @params: TokenList of an expression in the infix order
+  // @return: returns TokenList of an expression in the postfix order
   static TokenList ToPostfix(TokenList tokens_to_convert);
+
+  /* Checks if the primary list of tokens begin with the secondary list of
+   * tokens
+   *
+   * Used by PQL for pattern a(_, "expr"_)
+   *
+   * @return: returns true if the sequence of tokens_primary begins with the
+   * sequence of tokens in tokens_secondary.
+   *
+   * Example: "ab*3+" (tokens_primary) ,
+   * "ab*" (tokens_secondary) returns true
+   */
+  static bool IsPatternStartWith(TokenList tokens_primary,
+                                 TokenList tokens_secondary);
+
+  /* Checks if the primary list of tokens end with the secondary list of
+   * tokens.
+   *
+   * Used by PQL for pattern a(_, _"expr")
+   *
+   * @return: returns true if the sequence of tokens_primary ends with the
+   * sequence of tokens in tokens_secondary.
+   *
+   * Example: "ab*cxy+*+k+d+" (tokens_primary), "kd+" (tokens_secondary) returns
+   * true.
+   */
+  static bool IsPatternEndWith(TokenList tokens_primary,
+                               TokenList tokens_secondary);
+
+  /* Checks if the primary list of tokens matches exactly the secondary list of
+   * tokens.
+			*
+   * Used by PQL for pattern a(_,"expr")
+			*
+   * @return: returns true if the sequence of tokens_primary is the exact
+   * sequence of tokens in tokens_secondary.
+   *
+   * Example: "ab*c+" (tokens_primary), "ab*c+" (tokens_secondary) returns
+   * true.
+   */
+  static bool IsPatternExact(TokenList tokens_primary,
+                             TokenList tokens_secondary);
+		
+		static bool IsPatternSubSet(TokenList tokens_primary,
+                              TokenList tokens_secondary);
+ private:
+  static bool Matches(TokenList tokens_first, TokenList tokens_second,
+                      size_t start, size_t end);
 };
-#endif	
+#endif
