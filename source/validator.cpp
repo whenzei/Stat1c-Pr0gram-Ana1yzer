30f9b0bc932c372485d948046d01c4781b66112f
Lau Kar Rui <karrui.lau@gmail.com>
2018-09-16 19:54:11 +0800

    refactor(Validator): change IsValidExpression to take in a TokenList
    this change is to prepare for validating of conditional expressions, which may have expressions and do not end with a semicolon. Thus, reading the whole conditional expression and separating the expressions and passing it to IsValidExpression is needed.
    
    related to #31

--- source/validator.cpp
+++ source/validator.cpp
@@ -115,47 +115,50 @@ bool Validator::IsValidCallReadPrint() {
   return true;
 }
 
-bool Validator::IsValidExpression() {
+bool Validator::IsValidExpression(TokenList tokens) {
   // expr: expr ‘+’ term | expr ‘-’ term | term
   // term: term ‘*’ factor | term ‘ / ’ factor | term ‘%’ factor | factor
   // factor : var_name | const_value | ‘(’ expr ‘)’
   // expression should always start with kDigit or kName
   // in the style v o v o v, where v is a variable or digit, and o is operator
+  if (tokens.empty()) {
+    return false;
+  }
   bool was_operator = false;
   stack<tt> parenthesis_stack;
-  ReadNextToken();
-  if (curr_token_.type != tt::kOpenParen && curr_token_.type != tt::kName &&
-      curr_token_.type != tt::kDigit) {
+  Token current = tokens[0];
+  if (current.type != tt::kOpenParen && current.type != tt::kName &&
+      current.type != tt::kDigit) {
     return false;
   }
 
-  if (curr_token_.type == tt::kOpenParen) {
+  if (current.type == tt::kOpenParen) {
     parenthesis_stack.push(tt::kOpenParen);
     // false truth since next variable after this shouldn't be an operator
     was_operator = true;
   }
 
-  while (ReadNextToken().type != tt::kSemicolon) {
+  for (int i = 1; i < tokens.size(); i++) {
+    current = tokens[i];
     if (was_operator) {
-      if (curr_token_.type != tt::kDigit && curr_token_.type != tt::kName &&
-          curr_token_.type != tt::kOpenParen) {
-        cout << "[EXPR INVALID], current token: " << curr_token_.value << endl;
+      if (current.type != tt::kDigit && current.type != tt::kName &&
+          current.type != tt::kOpenParen) {
+        cout << "[EXPR INVALID], current token: " << current.value << endl;
         return false;
       }
 
-      if (curr_token_.type == tt::kOpenParen) {
+      if (current.type == tt::kOpenParen) {
         parenthesis_stack.push(tt::kOpenParen);
       } else {
         was_operator = false;
       }
     } else {
-      if (curr_token_.type != tt::kCloseParen &&
-          curr_token_.type != tt::kOperator) {
-        cout << "[EXPR INVALID], current token: " << curr_token_.value << endl;
+      if (current.type != tt::kCloseParen && current.type != tt::kOperator) {
+        cout << "[EXPR INVALID], current token: " << current.value << endl;
         return false;
       }
 
-      if (curr_token_.type == tt::kCloseParen) {
+      if (current.type == tt::kCloseParen) {
         if (parenthesis_stack.empty()) {
           cout << "[STACK NOT EMPTY -- INVALID]" << endl;
           return false;
@@ -184,7 +187,17 @@ bool Validator::IsValidAssignment() {
     cout << "[ASSIGN SYNTAX INVALID]" << endl;
     return false;
   }
-  return IsValidExpression();
+
+  TokenList expr;
+  while (PeekNextToken().type != tt::kSemicolon && !IsAtEnd()) {
+	  expr.push_back(ReadNextToken());
+  }
+  
+  if (!IsValidExpression(expr)) {
+	  return false;
+  }
+
+  return Match(tt::kSemicolon);
 }
 
 bool Validator::IsValidConditional() {
--- source/validator.h
+++ source/validator.h
@@ -19,7 +19,7 @@ public:
 	bool IsValidStmtList();
 	bool IsValidStatement();
 	bool IsValidAssignment();
-	bool IsValidExpression();
+	bool IsValidExpression(TokenList expr);
 	bool IsValidConditional();
 	bool IsValidIfBlock();
 	bool IsValidWhileBlock();
