d689b9edd9b70649b52a3504b80617fa9783e483
sophie <e0008892@u.nus.edu>
2018-09-19 21:45:51 +0800

    perf(PKB): change return types in all classes from List to Vector

--- source/stmt_type_list.h
+++ source/stmt_type_list.h
@@ -12,11 +12,11 @@ using std::list;
 using std::vector;
 
 // StmtNum is defined as a string inside PKB, because query results have to be
-// in the format list<string>. For consistency, all StmtNum inside PKB is string
+// in the format vector<string>. For consistency, all StmtNum inside PKB is string
 // so that it is easier to search and update the PKB data structures.
 using StmtNum = string;
-using StmtNumList = list<string>;
-using StmtTypeVector = vector<list<string>>;
+using StmtNumList = vector<string>;
+using StmtTypeVector = vector<vector<string>>;
 
 // The statement type list class for the PKB component
 // Used to store the statement numbers that belong to each statement type (e.g. assign, if)
--- source/stmtlist_table.h
+++ source/stmtlist_table.h
@@ -6,19 +6,21 @@
 #include <list>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 using std::list;
 using std::string;
 using std::pair;
 using std::unordered_map;
+using std::vector;
 
 // StmtNum is defined as a string inside PKB, because query results have to be
-// in the format list<string>. For consistency, all StmtNum inside PKB is string
+// in the format vector<string>. For consistency, all StmtNum inside PKB is string
 // so that it is easier to search and update the PKB data structures.
 using StmtNum = string;
-using StmtNumList = list<string>;
+using StmtNumList = vector<string>;
 using StmtListIndex = int;
-using StmtListMap = unordered_map<int, list<string>>;
+using StmtListMap = unordered_map<int, vector<string>>;
 
 // The statement list table class for the PKB component
 // Used to store statement list indices and corresponding statement numbers
