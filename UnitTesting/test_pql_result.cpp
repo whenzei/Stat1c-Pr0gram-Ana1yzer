c41b000879cf49e67a29426fc1c9615d2c863ec9
KEVIN CHIN <kevinchinjh@gmail.com>
2018-09-22 16:52:49 +0800

    feat(test_pql_result): add last test to test merge with two conflict

--- UnitTesting/test_pql_result.cpp
+++ UnitTesting/test_pql_result.cpp
@@ -39,6 +39,10 @@ TEST_CLASS(TestPqlResult) {
     pql_result.InitTable(test_result_list);
 
     ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have one column
+    Assert::IsTrue(result_row_column.size() == 1);
 
     // Should have three rows
     Assert::IsTrue(result_table.size() == 3);
@@ -62,6 +66,10 @@ TEST_CLASS(TestPqlResult) {
     pql_result.InitTable(test_resultpair_list);
 
     ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have two column
+    Assert::IsTrue(result_row_column.size() == 2);
 
     // Should have three rows
     Assert::IsTrue(result_table.size() == 3);
@@ -97,6 +105,10 @@ TEST_CLASS(TestPqlResult) {
     pql_result.MergeResults(test_merge_list, kNoConflict, -1);
 
     ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have two column
+    Assert::IsTrue(result_row_column.size() == 2);
 
     // Should have nine rows
     Assert::IsTrue(result_table.size() == 9);
@@ -137,6 +149,10 @@ TEST_CLASS(TestPqlResult) {
     pql_result.MergeResults(test_mergepair_list, kNoConflict, -1, -1);
 
     ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have three column
+    Assert::IsTrue(result_row_column.size() == 3);
 
     // Should have nine rows
     Assert::IsTrue(result_table.size() == 9);
@@ -188,6 +204,10 @@ TEST_CLASS(TestPqlResult) {
     pql_result.MergeResults(test_merge_list, kConflict, 0);
 
     ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have two column
+    Assert::IsTrue(result_row_column.size() == 2);
 
     // Should have two rows
     Assert::IsTrue(result_table.size() == 2);
@@ -225,6 +245,10 @@ TEST_CLASS(TestPqlResult) {
     pql_result.MergeResults(test_mergepair_list, kOneConflictLeft, 0, -1);
 
     ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have three column
+    Assert::IsTrue(result_row_column.size() == 3);
 
     // Should have two rows
     Assert::IsTrue(result_table.size() == 2);
@@ -268,6 +292,10 @@ TEST_CLASS(TestPqlResult) {
     pql_result.MergeResults(test_mergepair_list, kOneConflictRight, -1, 1);
 
     ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have three column
+    Assert::IsTrue(result_row_column.size() == 3);
 
     // Should have two rows
     Assert::IsTrue(result_table.size() == 2);
@@ -291,6 +319,47 @@ TEST_CLASS(TestPqlResult) {
     Assert::AreEqual(expected_resultc, result_table[1][1]);
     // Sec row last col should contain "p"
     Assert::AreEqual(expected_resultp, result_table[1][2]);
+  }
+
+  TEST_METHOD(TestMergeTableWithTwoConflict) {
+    PqlResult pql_result;
+
+    test_resultpair_list.push_back(std::make_pair("1", "a"));
+    test_resultpair_list.push_back(std::make_pair("2", "b"));
+    test_resultpair_list.push_back(std::make_pair("3", "c"));
+    test_resultpair_list.push_back(std::make_pair("4", "d"));
+    test_resultpair_list.push_back(std::make_pair("5", "e"));
+
+    pql_result.InitTable(test_resultpair_list);
+
+    test_mergepair_list.push_back(std::make_pair("d", "4"));
+    test_mergepair_list.push_back(std::make_pair("b", "2"));
+
+    // Conflict with column 0 & 1
+    pql_result.MergeResults(test_mergepair_list, kTwoConflict, 1, 0);
+
+    ResultTable result_table = pql_result.GetResultTable();
+    ResultRow result_row_column = result_table.front();
+
+    // Should have two column
+    Assert::IsTrue(result_row_column.size() == 2);
+
+    // Should have two rows
+    Assert::IsTrue(result_table.size() == 2);
+
+    string expected_result2 = "2";
+    string expected_result4 = "4";
+    string expected_resultb = "b";
+    string expected_resultd = "d";
+
+    // First row first col should contain "2"
+    Assert::AreEqual(expected_result2, result_table[0][0]);
+    // First row sec col should contain "b"
+    Assert::AreEqual(expected_resultb, result_table[0][1]);
+    // Sec row first col should contain "4"
+    Assert::AreEqual(expected_result4, result_table[1][0]);
+    // Sec row sec col should contain "d"
+    Assert::AreEqual(expected_resultd, result_table[1][1]);
   }
 };
 }  // namespace PQLTests
