62dafae050bcf221699b1dd61f2afebd58fbbf15
KEVIN CHIN <kevinchinjh@gmail.com>
2018-09-22 14:39:50 +0800

    feat(test_pql_result):add two test for merging of conflicting pair

--- UnitTesting/test_pql_result.cpp
+++ UnitTesting/test_pql_result.cpp
@@ -206,5 +206,91 @@ TEST_CLASS(TestPqlResult) {
     // Sec row sec col should contain "e"
     Assert::AreEqual(expected_resulte, result_table[1][1]);
   }
+
+  TEST_METHOD(TestMergeTableWithOneConflictLeftPair) {
+    PqlResult pql_result;
+
+    test_resultpair_list.push_back(std::make_pair("1", "a"));
+    test_resultpair_list.push_back(std::make_pair("2", "b"));
+    test_resultpair_list.push_back(std::make_pair("3", "c"));
+    test_resultpair_list.push_back(std::make_pair("4", "d"));
+    test_resultpair_list.push_back(std::make_pair("5", "e"));
+
+    pql_result.InitTable(test_resultpair_list);
+
+    test_mergepair_list.push_back(std::make_pair("1", "x"));
+    test_mergepair_list.push_back(std::make_pair("5", "y"));
+
+    // Conflict with column 0
+    pql_result.MergeResults(test_mergepair_list, kOneConflictLeft, 0, -1);
+
+    ResultTable result_table = pql_result.GetResultTable();
+
+    // Should have two rows
+    Assert::IsTrue(result_table.size() == 2);
+
+    string expected_result1 = "1";
+    string expected_result5 = "5";
+    string expected_resulta = "a";
+    string expected_resulte = "e";
+    string expected_resultx = "x";
+    string expected_resulty = "y";
+
+    // First row first col should contain "1"
+    Assert::AreEqual(expected_result1, result_table[0][0]);
+    // First row sec col should contain "a"
+    Assert::AreEqual(expected_resulta, result_table[0][1]);
+    // First row last col should contain "x"
+    Assert::AreEqual(expected_resultx, result_table[0][2]);
+    // Sec row first col should contain "5"
+    Assert::AreEqual(expected_result5, result_table[1][0]);
+    // Sec row sec col should contain "e"
+    Assert::AreEqual(expected_resulte, result_table[1][1]);
+    // Sec row last col should contain "y"
+    Assert::AreEqual(expected_resulty, result_table[1][2]);
+  }
+
+  TEST_METHOD(TestMergeTableWithOneConflictRightPair) {
+    PqlResult pql_result;
+
+    test_resultpair_list.push_back(std::make_pair("1", "a"));
+    test_resultpair_list.push_back(std::make_pair("2", "b"));
+    test_resultpair_list.push_back(std::make_pair("3", "c"));
+    test_resultpair_list.push_back(std::make_pair("4", "d"));
+    test_resultpair_list.push_back(std::make_pair("5", "e"));
+
+    pql_result.InitTable(test_resultpair_list);
+
+    test_mergepair_list.push_back(std::make_pair("s", "b"));
+    test_mergepair_list.push_back(std::make_pair("p", "c"));
+
+    // Conflict with column 0
+    pql_result.MergeResults(test_mergepair_list, kOneConflictRight, -1, 1);
+
+    ResultTable result_table = pql_result.GetResultTable();
+
+    // Should have two rows
+    Assert::IsTrue(result_table.size() == 2);
+
+    string expected_result2 = "2";
+    string expected_result3 = "3";
+    string expected_resultb = "b";
+    string expected_resultc = "c";
+    string expected_results = "s";
+    string expected_resultp = "p";
+
+    // First row first col should contain "2"
+    Assert::AreEqual(expected_result2, result_table[0][0]);
+    // First row sec col should contain "b"
+    Assert::AreEqual(expected_resultb, result_table[0][1]);
+    // First row last col should contain "s"
+    Assert::AreEqual(expected_results, result_table[0][2]);
+    // Sec row first col should contain "3"
+    Assert::AreEqual(expected_result3, result_table[1][0]);
+    // Sec row sec col should contain "c"
+    Assert::AreEqual(expected_resultc, result_table[1][1]);
+    // Sec row last col should contain "p"
+    Assert::AreEqual(expected_resultp, result_table[1][2]);
+  }
 };
 }  // namespace PQLTests
