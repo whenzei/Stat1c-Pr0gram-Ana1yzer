173bfe3e86fa23677ba3d5fb2e89c933c29ddf05
Mingrui Tan <mingtui.tan@yahoo.com>
2018-10-22 21:19:45 +0800

    refactor(PKB): change storage to int in VarList, add methods to get VarName from index and vice versa

--- Code08/source/var_list.cpp
+++ Code08/source/var_list.cpp
@@ -3,10 +3,10 @@
 #include "var_list.h"
 
 int VarList::InsertVarName(VarName var_name) {
-  if (var_name_set_.insert(var_name).second) {
+  if (var_index_map_.find(var_name) == var_index_map_.end()) {
     int index = var_count;
-    var_name_list_.push_back(var_name);
-    var_name_twin_list_.push_back(make_pair(var_name, var_name));
+    var_name_index_list_.push_back(index);
+    var_name_index_twin_list_.push_back(make_pair(index, index));
     var_index_map_[var_name] = index;
     index_var_map_[index] = var_name;
     var_count++;
@@ -15,16 +15,16 @@ int VarList::InsertVarName(VarName var_name) {
   return -1;
 }
 
-VarNameList VarList::GetAllVarName() {
-  return var_name_list_;
+VarNameIndexList VarList::GetAllVarName() {
+  return var_name_index_list_;
 }
 
 bool VarList::IsVarName(VarName var_name) {
-  return var_name_set_.find(var_name) != var_name_set_.end(); 
+  return var_index_map_.find(var_name) != var_index_map_.end(); 
 }
 
-VarNamePairList VarList::GetAllVarNameTwin() {
-  return var_name_twin_list_;
+VarNameIndexPairList VarList::GetAllVarNameTwin() {
+  return var_name_index_twin_list_;
 }
 
 IndexVarMap VarList::GetIndexToVarMapping() {
@@ -32,5 +32,10 @@ IndexVarMap VarList::GetIndexToVarMapping() {
 }
 
 VarIndexMap VarList::GetVarToIndexMapping() {
-  return var_index_map_;
+  return var_index_map_; }
+
+VarName VarList::GetVarName(VarNameIndex index) { return index_var_map_[index]; }
+
+VarNameIndex VarList::GetVarNameIndex(VarName var_name) {
+  return var_index_map_[var_name];
 }
--- Code08/source/var_list.h
+++ Code08/source/var_list.h
@@ -8,6 +8,7 @@
 #include <unordered_set>
 #include <vector>
 
+using std::make_pair;
 using std::pair;
 using std::string;
 using std::unordered_map;
@@ -15,18 +16,17 @@ using std::unordered_set;
 using std::vector;
 
 using VarName = string;
-using VarNameList = vector<VarName>;
-using VarNameSet = unordered_set<VarName>;
-using VarNamePairList = vector<pair<VarName, VarName>>;
-using VarIndexMap = unordered_map<VarName, int>;
-using IndexVarMap = unordered_map<int, VarName>;
+using VarNameIndex = int;
+using VarNameIndexList = vector<VarNameIndex>;
+using VarNameIndexPairList = vector<pair<VarNameIndex, VarNameIndex>>;
+using VarIndexMap = unordered_map<VarName, VarNameIndex>;
+using IndexVarMap = unordered_map<VarNameIndex, VarName>;
 
 // The variable list class for the PKB component
 // Used to store variable names that are passed into PKB from the parser
 class VarList {
-  VarNameList var_name_list_;
-  VarNameSet var_name_set_;
-  VarNamePairList var_name_twin_list_;
+  VarNameIndexList var_name_index_list_;
+  VarNameIndexPairList var_name_index_twin_list_;
   // TODO: Depending on PQL's preferences, might have to add below
   // a list of Proc Indices and just return the entire list.
   VarIndexMap var_index_map_;
@@ -38,10 +38,10 @@ class VarList {
 
  public:
   // Returns a list of all variable names in the VarList.
-  VarNameList GetAllVarName();
+  VarNameIndexList GetAllVarName();
 
   // Returns a list of all variable names in pairs (in each pair, the same var_name is repeated)
-  VarNamePairList GetAllVarNameTwin();
+  VarNameIndexPairList GetAllVarNameTwin();
 
   // Inserts var_name into VarList
   // @return index of the inserted variable if successful
@@ -56,6 +56,12 @@ class VarList {
 
   // @returns an unordered map with Var to Index mapping
   VarIndexMap GetVarToIndexMapping();
+
+  // @returns the corresponding var name
+  VarName GetVarName(VarNameIndex index);
+
+  // @returns the corresponding var name index
+  VarNameIndex GetVarNameIndex(VarName var_name);
 };
 
 #endif !SPA_VAR_LIST_H
