b5818a1a9f978f8a636badfecbbe766ecd6c9a61
KEVIN CHIN <kevinchinjh@gmail.com>
2018-10-26 11:57:31 +0800

    fix(pql_result): fix bug for merging with 2 synonyms

--- Code08/source/pql_result.cpp
+++ Code08/source/pql_result.cpp
@@ -142,8 +142,10 @@ void PqlResult::MergeResults(QueryResultPairList result_pair_list,
     MergeSet merge_set = GetMergeSet();
     // Iterating through the row
     for (auto& row : result_table) {
-      string unique_key =
-          row[conflict_left_pair] + "-" + row[conflict_right_pair];
+      string unique_key = std::to_string(row[conflict_left_pair]) + "-" +
+                          std::to_string(row[conflict_right_pair]);
+
+      cout << unique_key << endl;
       // If the column value in table matches the value in result list
       if (merge_set.count(unique_key)) {
         // Row has passed the comparison
@@ -204,7 +206,7 @@ void PqlResult::SetupMergeSet(QueryResultPairList result_pair_list) {
   ClearMergeSet();
 
   for (auto& iter : result_pair_list) {
-    AddMergeSet(iter.first + "-" + iter.second);
+    AddMergeSet(std::to_string(iter.first) + "-" + std::to_string(iter.second));
   }
 }
 
@@ -248,11 +250,8 @@ void PqlResult::AddMergeMap(int key, int value) {
   MergeMap::iterator iter = merge_map_.find(key);
 
   if (iter != merge_map_.end()) {
-    // cout << "Extra item (" << key << "," << value << ")" << endl;
-    // cout << "Extra Size " << iter->second.size() << endl;
     iter->second.push_back(value);
   } else {
-    // cout << "Unique item (" << key << "," << value << ")" << endl;
     // Create new list
     vector<int> merge_map_list;
     merge_map_list.push_back(value);
