cc9e1bb8887f4018df681c7468e46bc9804d2582
Lau Kar Rui <karrui.lau@gmail.com>
2018-10-27 23:08:28 +0800

    docs(PqlPreprocessor): add APIs to headers

--- Code08/source/pql_suchthat.h
+++ Code08/source/pql_suchthat.h
@@ -5,26 +5,33 @@
 
 #include <string>
 #include "pql_clause.h"
-using std::string;
 using std::pair;
+using std::string;
 
 #include "pql_global.h"
 
 /* Stores information of a such that clause in a PQL select statement */
 class PqlSuchthat : public PqlClause {
-private:
+ private:
   /* The relationship type of the such that clause */
   PqlSuchthatType type_;
   /* The parameters, stored in the form of ((synonym, type), (synonym, type)) */
   Parameters parameters_;
 
-public:
-  PqlSuchthat(PqlSuchthatType, string, PqlDeclarationEntity, string, PqlDeclarationEntity);
+ public:
+  PqlSuchthat(PqlSuchthatType, string, PqlDeclarationEntity, string,
+              PqlDeclarationEntity);
 
+  // @returns the type of the "such that" clause
   PqlSuchthatType GetType();
+
+  // @returns the parameters of the clause
   Parameters GetParameters();
+
+  // @returns a PqlSuchThatType enum from the given string
   static PqlSuchthatType StringToType(string);
 
+  // @returns the type of the clause
   PqlClauseType GetClauseType();
 };
 
