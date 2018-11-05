ac393e13e272a8b5a931a8740a360db615ccde0c
Lau Kar Rui <karrui.lau@gmail.com>
2018-11-05 19:23:14 +0800

    refactor(PqlExtractor): extract class NextExtractor
    same as previous commit, refer to #83

---
+++ Code08/source/next_extractor.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#ifndef NEXT_EXTRACTOR_H
+#define NEXT_EXTRACTOR_H
+
+#include "pkb.h"
+
+class NextExtractor {
+  PKB* pkb_;
+
+  //@params: start is the StmtNum that should be in the LHS of all pairs
+  //@params: res_list is passed by reference
+  void FormPairBFS(StmtNum start, StmtNumPairList* res_list);
+
+ public:
+  NextExtractor();
+  NextExtractor(PKB* pkb);
+
+  // @returns true if Next*(previous_stmt, next_stmt) holds
+  bool IsNextT(StmtNum previous_stmt, StmtNum next_stmt);
+
+  // @returns true if Next*(_, stmt_num) holds
+  bool IsNextT(StmtNum stmt_num);
+
+  // @returns true if Next*(stmt_num, _) holds
+  bool IsPreviousT(StmtNum stmt_num);
+
+  // @returns a list of all n's that satisfy Next*(stmt_num, n)
+  StmtNumList GetNextT(StmtNum stmt_num);
+
+  // @returns a list of all n's that satisfy Next*(n, stmt_num)
+  StmtNumList GetPreviousT(StmtNum stmt_num);
+
+  // @returns a list of all pairs of <n1, n2> that satisfy Next*(n1, n2)
+  StmtNumPairList GetAllNextTPairs();
+};
+
+#endif  // !NEXT_EXTRACTOR_H
